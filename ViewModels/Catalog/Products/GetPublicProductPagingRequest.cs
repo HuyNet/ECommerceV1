fdbfdaa10948fb8781da82f97b6752877493c8b7
HuyNet <116165375+HuyNet@users.noreply.github.com>
2023-12-24 15:54:14 +0700

    Add Image product API

--- ViewModels/Catalog/Products/GetPublicProductPagingRequest.cs
+++ ViewModels/Catalog/Products/GetPublicProductPagingRequest.cs
@@ -9,7 +9,7 @@ namespace ViewModels.Catalog.Products
 {
     public class GetPublicProductPagingRequest : PagingRequestBase
     {
-        public string languageId {  get; set; }
+        //public string languageId {  get; set; }
         public int? CategoryId { get; set; }
     }
 }
